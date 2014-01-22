5d09366be52be537f4a9a095deb9a2bca2fbf498
cloudera <cloudera@localhost.localdomain>
2014-01-22 10:39:22 -0800

    minor

---
+++ src/main/java/com/cloudera/avro/WriteAvroMapOnlyJob.java
@@ -0,0 +1,79 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.conf.Configuration;
+
+import org.apache.avro.mapred.AvroKey;
+
+import org.apache.avro.mapreduce.AvroJob;
+import org.apache.avro.mapreduce.AvroKeyOutputFormat;
+
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Job;
+
+import org.apache.avro.generic.GenericRecord;
+
+import example.avro.DistData;
+
+public class WriteAvroMapOnlyJob {
+
+  public static void main(String[] args) throws Exception {
+
+    /*
+     * The expected command-line arguments are the paths containing
+     * input and output data. Terminate the job if the number of
+     * command-line arguments is not exactly 2.
+     */
+    if (args.length != 2) {
+      System.out.printf(
+          "Usage: WriteAvroMapOnly <input dir> <output dir>\n");
+      System.exit(-1);
+    }
+
+    /*
+     * Instantiate a Job object for your job's configuration.  
+     */
+    Job job = new Job();
+    
+    /*
+     * Specify the jar file that contains your driver, mapper, and reducer.
+     * Hadoop will transfer this jar file to nodes in your cluster running
+     * mapper and reducer tasks.
+     */
+    job.setJarByClass(WriteAvroMapOnlyJob.class);
+    
+    /*
+     * Specify an easily-decipherable name for the job.
+     * This job name will appear in reports and logs.
+     */
+    job.setJobName("Read text write avro map only job");
+
+    /*
+     * Specify the paths to the input and output data based on the
+     * command-line arguments.
+     */
+    FileInputFormat.setInputPaths(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    
+    /*
+     * Specify the mapper and reducer classes.
+     */
+    job.setMapperClass(WriteAvroMapper.class);
+    AvroJob.setOutputKeySchema(job, DistData.getClassSchema());
+    job.setOutputFormatClass(AvroKeyOutputFormat.class);
+    job.setOutputKeyClass(AvroKey.class);
+    job.setOutputValueClass(NullWritable.class);
+    FileOutputFormat.setCompressOutput(job, true);
+    job.setNumReduceTasks(0);
+
+    /*
+     * Start the MapReduce job and wait for it to finish.
+     * If it finishes successfully, return 0. If not, return 1.
+     */
+    boolean success = job.waitForCompletion(true);
+    System.exit(success ? 0 : 1);
+  }
+}
---
+++ src/main/java/com/cloudera/avro/WriteAvroMapper.java
@@ -0,0 +1,58 @@
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.io.File;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import org.apache.avro.mapred.AvroKey;
+import org.apache.avro.mapreduce.AvroJob;
+import org.apache.avro.mapreduce.AvroKeyOutputFormat;
+
+import example.avro.DistData;
+
+/* 
+ * To define a map function for your MapReduce job, subclass 
+ * the Mapper class and override the map method.
+ * The class definition requires four parameters: 
+ *   The data type of the input key
+ *   The data type of the input value
+ *   The data type of the output key (which is the input key type 
+ *   for the reducer)
+ *   The data type of the output value (which is the input value 
+ *   type for the reducer)
+ */
+
+public class WriteAvroMapper extends Mapper<LongWritable, Text, AvroKey<DistData>, NullWritable> {
+  
+  /*
+   * The map method runs once for each line of text in the input file.
+   * The method receives a key of type LongWritable, a value of type
+   * Text, and a Context object.
+   */
+  @Override
+  public void map(LongWritable key, Text value, Context context)
+      throws IOException, InterruptedException {
+
+    /*
+     * Convert the line, which is received as a Text object,
+     * to a String object.
+     */
+    String line = value.toString();
+    String[] parts = line.split(" ");
+
+    AvroKey<DistData> outputKey = new AvroKey<DistData>(new DistData());
+
+    outputKey.datum().setName(parts[0]);
+    outputKey.datum().setFavoriteColor(parts[2]);
+    outputKey.datum().setFavoriteNumber(Integer.parseInt(parts[1]));
+
+    context.write(outputKey, null);
+      
+  }
+
+}
